43621cb40d00b2294093f2bca7edb76ad9f4ee0d
zahid-sm <89151692+zahid-sm@users.noreply.github.com>
2022-07-22 10:22:39 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,30 +6,41 @@ const show_time = document.getElementById('show_time')
 form_data.onsubmit = (e) => {
     e.preventDefault();
 
-   //get form data
+   let count = setInterval( () => {
+    //get form data
     let form_Data = new FormData(e.target)
     let {date , time} = Object.fromEntries(form_Data.entries())
     
     //get timestamps
-    let start_time = Date.now();
-    let end_time = new Date(date +''+ time);
-    let order_time = end_time.getTime() - start_time;
-
-
-    console.log(time);
-
-
-    
+    const start_time = Date.now();
+    const end_time = new Date(date + ' ' + time);
+    const order_time = Math.floor(Math.abs(end_time.getTime() - start_time));
 
 
 
-    //get Minite
-    let minite = start_time /1000;
-    //get Hour
-    let hour =  minite /60 ;
-    //get Day
-    let day =  hour / 24 ;
+
+    //get sec
+    let total_sec = Math.floor(order_time /1000);
+    // //get minite
+    let total_minite = Math.floor(total_sec /60);
+    // //get Hour
+    let total_hour = Math.floor(total_minite /60);
+    //Get Day
+    let total_dey = Math.floor(total_hour /24);
+
 
    
+    let hours = total_hour - (total_dey * 24 );
+    let min  = total_minite - (total_dey * 24 * 60) - (hours * 60);
+    let sec = total_sec - (total_dey * 24 * 60 * 60) - (hours * 60 * 60) - (min * 60);
+
+    if (total_sec <= 0) {
+        clearInterval(count)
+    }
+
+    show_time.innerHTML = `<h3> ${total_dey} Days - ${hours} Hours - ${min} Min - ${sec} Sec</h3>`
+
+
+   },1000);
     
 }
